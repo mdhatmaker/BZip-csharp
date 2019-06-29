3a16d96c8856ce266966b627cdf1a36f77ce9908
Michael Hatmaker <mhatmaker@gmail.com>
2019-06-28 22:02:05 -0500

    initial check-in

---
+++ BzipTest/BzipTest/BzipWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace BzipTest
+{
+    public class BzipWriter
+    {
+        FileStream _fs;
+        BZip2Stream _bzipStream;
+        StreamWriter _writer;
+
+        public BzipWriter(string pathname, BZip2CompressionLevel compressionLevel = BZip2CompressionLevel.Default)
+        {
+            _fs = new FileStream(pathname, FileMode.Append);
+            _bzipStream = new BZip2Stream(_fs, compressionLevel);
+            _writer = new StreamWriter(_bzipStream);
+        }
+
+        public void WriteLine(string text)
+        {
+            _writer.WriteLine(text);
+        }
+
+        public void Close()
+        {
+            _writer.Flush();
+            _writer.Close();
+        }
+
+    } // END OF CLASS BzipWriter
+
+} // END OF NAMESPACE
---
+++ BzipTest/BzipTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BzipTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\users\\mhatmaker\\test.bz2";
+            var bz = new BzipWriter(path);
+
+            int iterations = 1000000;
+            Console.WriteLine("Performing {0:#,###,###} writelines to compressed file...", iterations);
+            for (int i = 0; i < iterations; ++i)
+            {
+                string addTextToFile = string.Format("{0}  Here is another log entry (iteration {1})", DateTime.Now, i);
+                bz.WriteLine(addTextToFile);
+            }
+
+            bz.Close();
+        }
+
+    } // END OF CLASS Program
+} // END OF NAMESPACE
